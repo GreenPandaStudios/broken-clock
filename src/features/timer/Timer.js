a2a352c783b44dd21cf70d0107f6198ded61db4d
August Miller <augustamiller77@gmail.com>
2021-11-04 12:47:00 -0400

    Wrong time randomly displayed

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -5,19 +5,42 @@ import ReactDOM from 'react-dom';
 export function Timer(){
 
     const dispatch = useDispatch();
-    const offset = useSelector(selectOffset);
+    const [offset, setOffset] = useState(0);
+    
+
     const [state, setState] = useState(0);
 
-    useEffect(()=>{setInterval(()=>setState(1+state), 500)})
+    useEffect(()=>{
+        setInterval(()=>setState(1+state), 500);
+        
+        
+    });
+    useEffect(()=>{
+        if (Math.random() > 0.5){
+            setOffset(0);
+        }
+        else{
+            setOffset(Math.random() * 9999999);
+        }
+        
+    }, []);
     
+
+
     function getDateString(){
 
-        dispatch(setOffset)
+        
+
+        let date  = new Date();
+        date.setMinutes(date.getMinutes() + offset % 60);
+        date.setSeconds(date.getSeconds() + (offset * 1.2) % 60);
+        date.setMonth(date.getMonth() + (offset * 5.6) % 12);
+        date.setFullYear(date.getFullYear() + (offset * 5.6) % 100);
 
 
-        return new Date().toString();
+        return date.toLocaleString();
     }
-
+    
 
     return (<div>
       {getDateString()}
